ed14f3aacfc687f80cc67a86d0c6a37767e5e7be
kennethraff <kraffelino@student.ciputra.ac.id>
2023-10-16 21:16:37 +0700

    Update home.dart
    final

--- Challenge/lib/home.dart
+++ Challenge/lib/home.dart
@@ -8,6 +8,14 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool isFavorite = false; // tombol ganti2 status favorit
+
+  void toggleFavorite() {
+    setState(() {
+      isFavorite = !isFavorite;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,8 +31,8 @@ class _HomePageState extends State<HomePage> {
                     begin: Alignment.centerLeft,
                     end: Alignment.centerRight,
                     colors: [
-                      const Color.fromARGB(255, 134, 204, 236),
-                      Colors.white
+                      Color.fromARGB(255, 0, 145, 236),
+                      Color.fromARGB(255, 242, 8, 0)
                     ],
                   ),
                 ),
@@ -123,54 +131,58 @@ class _HomePageState extends State<HomePage> {
                       ),
                     ),
                     Flexible(
-                      flex: 4,
-                      child: Column(
-                        children: [
-                          Text("Star Wars: The Last Jedi"
-                          
-                          ,style: TextStyle(
-                            fontFamily: "Roboto",
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black
-                          ),),
-                          Expanded(
-                        child: SingleChildScrollView(
-                          clipBehavior: Clip.antiAlias,
-                          padding: EdgeInsets.all(8),
-                          child: Text(
-                            "Star Wars: The Last Jedi (also known as Star Wars: Episode VIII – The Last Jedi) is a 2017 American epic space opera film written and directed by Rian Johnson. Produced by Lucasfilm and distributed by Walt Disney Studios Motion Pictures, it is the second installment of the Star Wars sequel trilogy, following The Force Awakens (2015), and the eighth episode of the nine-part Skywalker saga. The film's ensemble cast includes Mark Hamill, Carrie Fisher, Adam Driver, Daisy Ridley, John Boyega, Oscar Isaac, Andy Serkis, Lupita Nyong'o, Domhnall Gleeson, Anthony Daniels, Gwendoline Christie, Kelly Marie Tran, Laura Dern, and Benicio del Toro. The Last Jedi follows Rey as she seeks the aid of Luke Skywalker in hopes of turning the tide for the Resistance in the fight against Kylo Ren and the First Order while General Leia Organa, Finn, and Poe Dameron attempt to escape a First Order attack on the dwindling Resistance fleet. The film features the first posthumous film performance by Fisher, who died in December 2016, and the film is dedicated to her memory",
-                            style: TextStyle(
-                              fontFamily: "Roboto",
-                                fontSize: 18,
-                                wordSpacing: 2,
-                                letterSpacing: 1,
-                                color: Colors.black
+                        flex: 4,
+                        child: Column(
+                          children: [
+                            Text(
+                              "Star Wars: The Last Jedi",
+                              style: TextStyle(
+                                  fontFamily: "Poppins",
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                            ),
+                            Expanded(
+                              child: SingleChildScrollView(
+                                clipBehavior: Clip.antiAlias,
+                                padding: EdgeInsets.all(8),
+                                child: Text(
+                                  "Star Wars: The Last Jedi (also known as Star Wars: Episode VIII – The Last Jedi) is a 2017 American epic space opera film written and directed by Rian Johnson. Produced by Lucasfilm and distributed by Walt Disney Studios Motion Pictures, it is the second installment of the Star Wars sequel trilogy, following The Force Awakens (2015), and the eighth episode of the nine-part Skywalker saga. The film's ensemble cast includes Mark Hamill, Carrie Fisher, Adam Driver, Daisy Ridley, John Boyega, Oscar Isaac, Andy Serkis, Lupita Nyong'o, Domhnall Gleeson, Anthony Daniels, Gwendoline Christie, Kelly Marie Tran, Laura Dern, and Benicio del Toro. The Last Jedi follows Rey as she seeks the aid of Luke Skywalker in hopes of turning the tide for the Resistance in the fight against Kylo Ren and the First Order while General Leia Organa, Finn, and Poe Dameron attempt to escape a First Order attack on the dwindling Resistance fleet. The film features the first posthumous film performance by Fisher, who died in December 2016, and the film is dedicated to her memory",
+                                  style: TextStyle(
+                                      fontFamily: "Poppins",
+                                      fontSize: 18,
+                                      color: Colors.white),
+                                  textAlign: TextAlign.left,
                                 ),
-                            textAlign: TextAlign.left,
-                          ),
-                        ),
-                      )
-                        ],
-                      )
-                    ),
+                              ),
+                            )
+                          ],
+                        )),
                   ],
                 ),
               ),
             ),
-            // Add a Positioned widget to place the FloatingActionButton in the top right corner.
-      Positioned(
-        top: 16.0, // Adjust the top position as needed.
-        right: 16.0, // Adjust the right position as needed.
-        child: FloatingActionButton(
-          onPressed: () {
-            // Add your favorite button functionality here.
-            // You can toggle the favorite status or perform any action.
-          },
-          child: Icon(Icons.favorite), // You can customize the icon.
-          backgroundColor: Colors.red, // You can customize the background color.
-        ),
-      ),
+            Positioned(
+              top: 15.0, //margin atas
+              right: 10.0, //margin kanan
+              child: FloatingActionButton(
+                onPressed: () {
+                  toggleFavorite(); // button favorit dari atas
+                },
+                child: Icon(
+                  isFavorite
+                      ? Icons.favorite
+                      : Icons
+                          .favorite_border,
+                  color: isFavorite
+                      ? Colors.red
+                      : Colors.black, 
+                ),
+                backgroundColor: isFavorite
+                    ? Colors.white
+                    : Colors.white60, //biar transparan 60%
+              ),
+            ),
           ],
         ));
   }
