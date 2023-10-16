123cb73069b5a74b4be7b57211a790ce43c2cdfb
kennethraff <kraffelino@student.ciputra.ac.id>
2023-10-16 16:40:26 +0700

    first
    dasar

---
+++ Challenge/android/app/src/main/kotlin/com/example/depd23_first/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.depd23_first
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Challenge/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:depd23_first/home.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: false,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
