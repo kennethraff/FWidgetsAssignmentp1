481276cfad6e37a975303a39f8df228770b73788
kennethraff <kraffelino@student.ciputra.ac.id>
2023-10-16 16:56:42 +0700

    Update home.dart
    bekgron

--- Challenge/lib/home.dart
+++ Challenge/lib/home.dart
@@ -10,7 +10,27 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Movie Detail')),
+        body: Stack(
+          children: [
+            SizedBox(
+              height: double.infinity,
+              width: double.infinity,
+              child: Container(
+                decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
+                  colors: [Colors.lightBlue.shade100, Colors.white],
+                ),
+              ),
+              ),
+            )
+          ],
+        )
+    );
     
   }
 }
